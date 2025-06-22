8ca02c853afe7f3e782500aaa096f7b9946748ae
munal777 <munalpoudel3@gmail.com>
2025-06-22 23:11:05 +0545

    routed done in asgi file

--- myproject/myproject/asgi.py
+++ myproject/myproject/asgi.py
@@ -10,10 +10,15 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from chat.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 
 application = ProtocolTypeRouter({  # 	This class Routes requests by protocol type
     "http": get_asgi_application(), # Django’s built-in function to return an ASGI application instance to handle HTTP requests
+    "websocket": AuthMiddlewareStack(
+        URLRouter(websocket_urlpatterns)
+    )
 })
