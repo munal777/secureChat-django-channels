ec4f4e53632cb412aab55859c0cb970327331eaa
munal777 <munalpoudel3@gmail.com>
2025-06-26 20:32:58 +0545

    added the connect method in the consumer

--- myproject/chat/consumers.py
+++ myproject/chat/consumers.py
@@ -1,4 +1,6 @@
 from channels.consumer import SyncConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
+import json
 
 class EchoConsumer(SyncConsumer):
 
@@ -22,39 +24,14 @@ class EchoConsumer(SyncConsumer):
             "text": response,
         })
 
-
-from channels.generic.websocket import AsyncWebsocketConsumer
-import json
-
 class ChatConsumer(AsyncWebsocketConsumer):
+    
     async def connect(self):
         self.room_group_name = "chatroom"
+
         await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
         )
+
         await self.accept()
-
-    async def disconnect(self, close_code):
-        await self.channel_layer.group_discard(
-            self.room_group_name,
-            self.channel_name
-        )
-
-    async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                'type': 'chat_message',
-                'message': message
-            }
-        )
-
-    async def chat_message(self, event):
-        message = event['message']
-        await self.send(text_data=json.dumps({
-            'message': message
-        }))
