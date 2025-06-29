bcbe7a91d017fad2482bd28e71bf0639f21f0713
munal777 <munalpoudel3@gmail.com>
2025-06-29 08:22:52 +0545

    defined the view for to pass the user list

--- myproject/chat/views.py
+++ myproject/chat/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-# Create your views here.
+from django.contrib.auth import get_user_model
+from .utils import make_room_name
+
+User = get_user_model()
 
 @login_required
 def chat_view(request, room_name):
@@ -8,3 +11,10 @@ def chat_view(request, room_name):
 
 
 
+def chat_user_list(request):
+    users = User.objects.exclude(id=request.user.id)
+
+    return render(request, "dashboard.html", {
+        "users": users,
+        "make_room_name": make_room_name,
+    })
