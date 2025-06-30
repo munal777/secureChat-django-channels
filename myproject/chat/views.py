c831c156d5ea944ffef0a1f4aae191dd8d2760d0
munal777 <munalpoudel3@gmail.com>
2025-06-30 15:26:29 +0545

    displayed the username for the chat

--- myproject/chat/views.py
+++ myproject/chat/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import get_user_model
 from .utils import make_room_name
@@ -7,7 +7,27 @@ User = get_user_model()
 
 @login_required
 def chat_view(request, room_name):
-    return render(request, 'chat.html', {'room_name': room_name})
+    room_members_id = room_name.split('_')
+
+    members_list = []
+
+    
+    try:
+        for member_id in room_members_id:
+            user = User.objects.get(id=int(member_id))
+
+            members_list.append(user.username)            
+
+    except User.DoesNotExist:
+        return redirect('dashboard')
+    
+    members_name = " & ".join(members_list)
+        
+
+    return render(request, 'chat.html', {
+        'room_name': room_name,
+        'room_member': members_name,
+    })
 
 
 @login_required
@@ -19,6 +39,7 @@ def dashboard_view(request):
         {
             "username": user.username,
             "room_name": make_room_name(current_user.id, user.id),
+            "room_member": f"{current_user.username} {user.username}"
         }
         for user in users
     ]
