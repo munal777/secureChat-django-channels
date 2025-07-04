f930a46fba380f2284ebaeb464c9a4301f25b876
munal777 <munalpoudel3@gmail.com>
2025-07-04 22:07:48 +0545

    changes on the ui design of the login and register page

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+2(ztamd_u#fv*i8107o8bi3sxchrfa=v*#q13r!_z^z!t6^*4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.0.101', '127.0.0.1']
 
 
 # Application definition
