92bcb40f83a7987938cdec515573375cac38479c
munal777 <munalpoudel3@gmail.com>
2025-07-05 22:19:23 +0545

    added the cryptography library to encrept/decrept the message

--- myproject/chat/utils.py
+++ myproject/chat/utils.py
@@ -1,5 +1,7 @@
 from channels.db import database_sync_to_async
 from .models import Message
+from django.conf import settings
+from cryptography.fernet import Fernet
 
 @database_sync_to_async
 def save_message(user, room_name, message):
@@ -8,3 +10,15 @@ def save_message(user, room_name, message):
 
 def make_room_name(id1, id2):
     return f"{min(id1, id2)}_{max(id1, id2)}"
+
+
+
+cipher = Fernet(settings.FERNET_SECRET_KEY)
+
+
+def encrypt_message(plain_text):
+    return cipher.encrypt(plain_text.encode()).decode()
+
+
+def decrypt_message(cipher_text):
+    return cipher.decrypt(cipher_text.encode()).decode()
