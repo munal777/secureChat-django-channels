8e36407c2cfa8b17ddfe8c81ee753705614a5d9f
munal777 <munalpoudel3@gmail.com>
2025-07-06 22:53:33 +0545

    add the view in the urls

--- myproject/chat/urls.py
+++ myproject/chat/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import chat_view, dashboard_view, landing_page_view
+from .views import chat_view, dashboard_view, landing_page_view, create_group_view
 
 urlpatterns = [
     path('chat/<str:room_name>/', chat_view, name='chat_room'),
     path('dashboard/', dashboard_view, name='dashboard'),
     path('', landing_page_view, name='landing_page'),
+    path('create-group/', create_group_view, name='create_group')
 ]
