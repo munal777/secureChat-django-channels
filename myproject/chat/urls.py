1a4a3bcfc9b5c5ab4cffc38e2e182dab913d6a43
munal777 <munalpoudel3@gmail.com>
2025-07-09 16:10:33 +0545

    implemented the leave group feature in the template also

--- myproject/chat/urls.py
+++ myproject/chat/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import chat_view, dashboard_view, landing_page_view, create_group_view
+from .views import chat_view, dashboard_view, landing_page_view, create_group_view, leave_group_view
 
 urlpatterns = [
     path('chat/<str:room_name>/', chat_view, name='chat_room'),
     path('dashboard/', dashboard_view, name='dashboard'),
     path('', landing_page_view, name='landing_page'),
-    path('create-group/', create_group_view, name='create_group')
+    path('create-group/', create_group_view, name='create_group'),
+    path('leave-group/', leave_group_view, name='leave_group'),
 ]
