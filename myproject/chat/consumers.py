9eb8a3f9a1e413567899172752f0f22bca825b15
munal777 <munalpoudel3@gmail.com>
2025-07-12 22:18:04 +0545

    updated the docstring for the group consumer

--- myproject/chat/consumers.py
+++ myproject/chat/consumers.py
@@ -106,6 +106,9 @@ class PrivateChatConsumer(AsyncWebsocketConsumer):
 
 
 class GroupChatConsumer(AsyncWebsocketConsumer):
+    """
+    Handles real-time group messaging between many users over WebSocket.
+    """
 
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
