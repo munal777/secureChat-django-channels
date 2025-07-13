90c21e71a7c8583fce038b719100f2d9d1d87d24
munal777 <munalpoudel3@gmail.com>
2025-07-13 15:06:50 +0545

    updateed the startup.sh

--- myproject/startup.sh
+++ myproject/startup.sh
@@ -1,14 +1,13 @@
+#!/bin/bash
+
 # Exit on error
 set -o errexit
 
 # Set Django settings module
 export DJANGO_SETTINGS_MODULE=myproject.settings
 
-
 echo "Running database migrations..."
 python manage.py migrate
 
-echo "Starting Django server..."
-
-PORT=${PORT:-8000}
-daphne -b 0.0.0.0 -p $PORT myproject.asgi:application
+echo "Starting Django server on 0.0.0.0:${PORT}..."
+daphne -b 0.0.0.0 -p ${PORT} myproject.asgi:application
