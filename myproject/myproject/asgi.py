8b74e7a1e01b86ff62ee9447df176a73937109d0
munal777 <munalpoudel3@gmail.com>
2025-07-13 11:33:29 +0545

    updated the startup and the render with the adding django_setting_module

--- myproject/myproject/asgi.py
+++ myproject/myproject/asgi.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
+import django
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
@@ -15,6 +16,8 @@ from channels.auth import AuthMiddlewareStack
 from chat.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
+
+django.setup()
 
 application = ProtocolTypeRouter({  # 	This class Routes requests by protocol type
     "http": get_asgi_application(), # Django’s built-in function to return an ASGI application instance to handle HTTP requests
--- myproject/startup.sh
+++ myproject/startup.sh
@@ -1,6 +1,10 @@
 # Exit on error
 set -o errexit
 
+# Set Django settings module
+export DJANGO_SETTINGS_MODULE=myproject.settings
+
+
 echo "Running database migrations..."
 python manage.py migrate
 
