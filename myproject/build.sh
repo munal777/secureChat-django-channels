d1ba1b40dfa94cac0e5cd4d982ed198c8f7cf59e
munal777 <munalpoudel3@gmail.com>
2025-07-13 14:37:42 +0545

    update the asgi.py

--- myproject/build.sh
+++ myproject/build.sh
@@ -1,4 +1,7 @@
 set -o errexit
 
+# Set Django settings module
+export DJANGO_SETTINGS_MODULE=myproject.settings
+
 # install python dependencies
 pip install -r requirements.txt
--- myproject/myproject/asgi.py
+++ myproject/myproject/asgi.py
@@ -10,14 +10,14 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 import os
 import django
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
+
+django.setup()
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from chat.routing import websocket_urlpatterns
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
-
-django.setup()
 
 application = ProtocolTypeRouter({  # 	This class Routes requests by protocol type
     "http": get_asgi_application(), # Django’s built-in function to return an ASGI application instance to handle HTTP requests
